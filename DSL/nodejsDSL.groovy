9bf6ea8430b8fd58af95358fea607b9fa118178b
nsanchezangulo <92123987+nsanchezangulo@users.noreply.github.com>
2022-01-18 09:51:48 -0500

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -10,7 +10,7 @@ job('Aplicacion Node.js DSL') {
         scm('H/7 * * * *')
     }
     wrappers {
-        nodejs('NodeJs')
+        nodejs('nodejs')
     }
     steps {
         shell("npm install")
